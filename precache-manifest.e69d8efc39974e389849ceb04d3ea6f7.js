3168ff0e84582cdd8786e505cc67c1d6c2e5b5b3
RedNesto <RedNestoMC@gmail.com>
2020-05-26 12:01:38 +0200

    Updates

---
+++ precache-manifest.e69d8efc39974e389849ceb04d3ea6f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "743f39f158033213d1b27125bfcbb6e8",
+    "url": "/turnips-tracker/index.html"
+  },
+  {
+    "revision": "0180a21dcb02f2c998f3",
+    "url": "/turnips-tracker/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "97c9ac5b1ba23500dd1d",
+    "url": "/turnips-tracker/static/js/2.0143d328.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/turnips-tracker/static/js/2.0143d328.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0180a21dcb02f2c998f3",
+    "url": "/turnips-tracker/static/js/main.0795bfb8.chunk.js"
+  },
+  {
+    "revision": "0d41b741a519b3d73ab7",
+    "url": "/turnips-tracker/static/js/runtime-main.dd9a08b0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/turnips-tracker/static/media/logo.5d5d9eef.svg"
+  }
+]);
