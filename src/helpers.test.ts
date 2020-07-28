120cccddc877ed990eaff388ae36395457810f95
RedNesto <rednestomc@gmail.com>
2020-07-28 12:01:03 +0200

    helpers.test: Use arrow functions for consistency

--- src/helpers.test.ts
+++ src/helpers.test.ts
@@ -6,25 +6,25 @@ it('should sort dates correctly for days', () => {
     expect(compareDates("2020-05-29", "2020-05-28")).toEqual(1)
 })
 
-it('should sort dates correctly for months', function () {
+it('should sort dates correctly for months', () => {
     expect(compareDates("2020-05-01", "2020-05-01")).toEqual(0)
     expect(compareDates("2020-04-01", "2020-05-01")).toEqual(-1)
     expect(compareDates("2020-05-01", "2020-04-01")).toEqual(1)
 })
 
-it('should sort dates correctly for years', function () {
+it('should sort dates correctly for years', () => {
     expect(compareDates("2020-05-01", "2020-05-01")).toEqual(0)
     expect(compareDates("2019-05-01", "2020-05-01")).toEqual(-1)
     expect(compareDates("2020-05-01", "2019-05-01")).toEqual(1)
 })
 
-it('should sort completely different dates correctly', function () {
+it('should sort completely different dates correctly', () => {
     expect(compareDates("2020-05-01", "2020-06-28")).toEqual(-1)
     expect(compareDates("2020-05-01", "2019-01-15")).toEqual(1)
     expect(compareDates("2021-10-20", "2020-10-01")).toEqual(1)
 })
 
-it('should handle malformed input', function () {
+it('should handle malformed input', () => {
     expect(compareDates("2020-05-28", "05-28")).toEqual(Number.MIN_SAFE_INTEGER)
     expect(compareDates("2020-05-28", "-05-28")).toEqual(Number.MIN_SAFE_INTEGER)
     expect(compareDates("2020-05-28", "-05-28")).toEqual(Number.MIN_SAFE_INTEGER)
